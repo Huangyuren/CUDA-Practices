84ac5d56e81e0d0115393f9af0e62b64ee7fda09
Huangyuren <forautodesk1@gmail.com>
2020-11-14 03:31:55 +0800

    [Fixed] matrixMultiplication work.

--- vector_mult_2D.cu
+++ vector_mult_2D.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE 32
 #define cudaCheckError() {                  \
     cudaError_t e = cudaGetLastError();     \
     if (e != cudaSuccess) {                 \
@@ -26,7 +26,7 @@ __global__ void matrixMultiplication(int* dev_a, int* dev_b, int* dev_c, int row
     int ret=0;
     if (row < row_a && col < col_b) {
         for(int i=0; i<col_a; ++i) {
-            ret += dev_a[row * col_a + i] * dev_b[i * col_b + col_b];
+            ret += dev_a[row * col_a + i] * dev_b[i * col_b + col];
         }
         dev_c[row*col_b + col] = ret;
     }
@@ -43,6 +43,17 @@ void matrixMultiplication_cpu(int* host_a, int* host_b, int* host_c, int row_a, 
         }
     }
 }
+bool verifyResult(int* h_c, int* h_c_result, int rows, int cols) {
+    for(int i=0; i<rows; ++i) {
+        for(int j=0; j<cols; ++j) {
+            if(h_c[i*cols + j] != h_c_result[i*cols + j]){
+                printf("Host: %d, Device: %d\n", h_c[i*cols + j], h_c_result[i*cols + j]);
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]) {
     if(argc != 4){
@@ -52,11 +63,11 @@ int main(int argc, char* argv[]) {
 	int row_a = atoi(argv[1]);
 	int col_a = atoi(argv[2]);
 	int col_b = atoi(argv[3]);
-	int* h_a, *h_b, *h_c, *h_c_for_dev;
-	cudaCheckError_inline(cudaMallocHost(&h_a, sizeof(int)*(row_a*col_a)));
-	cudaCheckError_inline(cudaMallocHost(&h_b, sizeof(int)*(col_a*col_b)));
-	cudaCheckError_inline(cudaMallocHost(&h_c, sizeof(int)*(row_a*col_b)));
-    cudaCheckError_inline(cudaMallocHost(&h_c_for_dev, sizeof(int)*(row_a*col_b)));
+	int* h_a, *h_b, *h_c, *h_c_result;
+	cudaCheckError_inline(cudaMallocHost((void **) &h_a, sizeof(int)*(row_a*col_a)));
+	cudaCheckError_inline(cudaMallocHost((void **) &h_b, sizeof(int)*(col_a*col_b)));
+	cudaCheckError_inline(cudaMallocHost((void **) &h_c, sizeof(int)*(row_a*col_b)));
+    cudaCheckError_inline(cudaMallocHost((void **) &h_c_result, sizeof(int)*(row_a*col_b)));
     //Random initialized matrix a on host
     for(int i=0; i<row_a; ++i) {
         for(int j=0; j<col_a; ++j) {
@@ -70,11 +81,11 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    float gpu_elapsed_time_ms, cpu_elapsed_time;
+    float gpu_elapsed_time_ms, cpu_elapsed_time_ms;
 	cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    // Start to count execution time of device computation
+    // Start counting execution time of device computation
     cudaEventRecord(start, 0);
 	int* dev_a, *dev_b, *dev_c;
 	cudaCheckError_inline(cudaMalloc((void **) &dev_a, sizeof(int)*(row_a*col_a)));
@@ -89,20 +100,38 @@ int main(int argc, char* argv[]) {
 	dim3 dimGrid(grid_col, grid_row);
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 	matrixMultiplication<<<dimGrid, dimBlock>>>(dev_a, dev_b, dev_c, row_a, col_a, col_b);
-    cudaCheckError();
-    cudaCheckError_inline(cudaMemcpy(h_c_for_dev, dev_c, sizeof(int)*row_a*col_b, cudaMemcpyDeviceToHost));
+    cudaCheckError_inline(cudaDeviceSynchronize());
+    cudaCheckError_inline(cudaMemcpy(h_c_result, dev_c, sizeof(int)*row_a*col_b, cudaMemcpyDeviceToHost));
     
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
-    printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n\n", row_a, col_a, col_a, col_b, gpu_elapsed_time_ms);
+    printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n", row_a, col_a, col_a, col_b, gpu_elapsed_time_ms);
+
+    //Start counting execution time of cpu computation
+    cudaEventRecord(start, 0);
     matrixMultiplication_cpu(h_a, h_b, h_c, row_a, col_a, col_b);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&cpu_elapsed_time_ms, start, stop);
+    printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on CPU: %f ms.\n", row_a, col_a, col_a, col_b, cpu_elapsed_time_ms);
+    bool check = verifyResult(h_c, h_c_result, row_a, col_b);
+    if(!check) {
+        fprintf(stderr, "Error, result not matched.\n");
+        exit(4);
+    }else{
+        printf("Congratulations, results match !!\n");
+    }
+    float speedups = cpu_elapsed_time_ms / gpu_elapsed_time_ms;
+    printf("Overall speedup = %f\n", speedups);
+
     cudaCheckError_inline(cudaFree(dev_a));
     cudaCheckError_inline(cudaFree(dev_b));
     cudaCheckError_inline(cudaFree(dev_c));
-    free(h_a);
-    free(h_b);
-    free(h_c);
-    free(h_c_for_dev);
+
+    cudaCheckError_inline(cudaFreeHost(h_a));
+    cudaCheckError_inline(cudaFreeHost(h_b));
+    cudaCheckError_inline(cudaFreeHost(h_c));
+    cudaCheckError_inline(cudaFreeHost(h_c_result));
 
 }
